edf99d2fd2979f4ec530aed1c3e06b83699185c8
robert hudek <hudektech@gmail.com>
2022-09-29 15:55:16 +0800

    3

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ let numClicked = document.querySelector("#num-clicked");
 let body = document.querySelector("body");
 let poppycatClose = document.querySelector("#poppycat-close");
 let poppycatOpen = document.querySelector("#poppycat-open");
+let audio = document.querySelector("#audio");
+
 let btnGreen = document.querySelector("#btn-green");
 let btnRed = document.querySelector("#btn-red");
 let btnBlue = document.querySelector("#btn-blue");
-let audio = document.querySelector("#audio");
+
+// Basic Option
+poppycatClose.addEventListener("click", (event) => {
+  poppycatClose.classList.add("no-display");
+  poppycatOpen.classList.remove("no-display");
+  audio.play();
+
+  num = num + 1;
+  numClicked.innerText = "被打了 " + num.toLocaleString() + "次";
+
+  setTimeout(() => {
+    poppycatClose.classList.remove("no-display");
+    poppycatOpen.classList.add("no-display");
+    audio.pause();
+    audio.currentTime = 0;
+  }, 1000);
+});
+
+btnGreen.addEventListener("click", (event) => {
+  body.style.backgroundColor = "aquamarine";
+});
+// });
+btnRed.addEventListener("click", (event) => {
+  body.style.backgroundColor = "lightcoral";
+});
+btnBlue.addEventListener("click", (event) => {
+  body.style.backgroundColor = "lightskyblue";
+});
 
 // // Advanced options
 // let poppycatOpenMouseUp = (event) => {
@@ -26,31 +55,3 @@ let audio = document.querySelector("#audio");
 
 //   poppycatOpen.addEventListener("mouseup", poppycatOpenMouseUp);
 // });
-
-// Basic Option
-poppycatClose.addEventListener("click", (event) => {
-  poppycatClose.classList.add("no-display");
-  poppycatOpen.classList.remove("no-display");
-  audio.play();
-
-  num = num + 1;
-  numClicked.innerText = "被打了 " + num.toLocaleString() + "次";
-
-  setTimeout(() => {
-    poppycatClose.classList.remove("no-display");
-    poppycatOpen.classList.add("no-display");
-    audio.pause();
-    audio.currentTime = 0;
-  }, 200);
-});
-
-btnGreen.addEventListener("click", (event) => {
-  body.style.backgroundColor = "aquamarine";
-});
-// });
-btnRed.addEventListener("click", (event) => {
-  body.style.backgroundColor = "lightcoral";
-});
-btnBlue.addEventListener("click", (event) => {
-  body.style.backgroundColor = "lightskyblue";
-});
